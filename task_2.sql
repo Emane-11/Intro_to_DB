4c6ecf5a11bc7eeff1fe38e0b5c5715587441c8f
Emane-11 <alhachimi2000@gmail.com>
2025-06-29 22:49:18 +0100

    Update task_2.sql

--- task_2.sql
+++ task_2.sql
@@ -1,13 +1,10 @@
--- Use the correct database
 USE alx_book_store;
 
--- Create the AUTHORS table
 CREATE TABLE IF NOT EXISTS AUTHORS (
     author_id INT AUTO_INCREMENT PRIMARY KEY,
     author_name VARCHAR(215) NOT NULL
 );
 
--- Create the BOOKS table
 CREATE TABLE IF NOT EXISTS BOOKS (
     book_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(130) NOT NULL,
@@ -17,7 +14,6 @@ CREATE TABLE IF NOT EXISTS BOOKS (
     FOREIGN KEY (author_id) REFERENCES AUTHORS(author_id)
 );
 
--- Create the CUSTOMERS table
 CREATE TABLE IF NOT EXISTS CUSTOMERS (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215) NOT NULL,
@@ -25,7 +21,6 @@ CREATE TABLE IF NOT EXISTS CUSTOMERS (
     address TEXT
 );
 
--- Create the ORDERS table
 CREATE TABLE IF NOT EXISTS ORDERS (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT,
@@ -33,7 +28,6 @@ CREATE TABLE IF NOT EXISTS ORDERS (
     FOREIGN KEY (customer_id) REFERENCES CUSTOMERS(customer_id)
 );
 
--- Create the ORDER_DETAILS table
 CREATE TABLE IF NOT EXISTS ORDER_DETAILS (
     orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
